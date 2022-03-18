445a4b3a31242e4f44301444e681abf9ff2b28fe
Shengyi Jiang <shengyi.jiang@outlook.com>
2022-03-18 23:27:18 +0800

    finish usub_elab_keeps_mono

--- src-lite/bidirectional.v
+++ src-lite/bidirectional.v
@@ -1,4 +1,5 @@
 Require Import Program.Tactics.
+Require Import Metalib.FSetWeakNotin.
 
 Require Import decl.properties.
 Require Import bidir.notations.
@@ -46,6 +47,73 @@ Proof.
   - admit.
 Admitted.
 
+Ltac solve_trivial_mono_or_lc :=
+  match goal with
+  | H : mono_type ?P |- _ => dependent destruction H; eauto; fail
+  | H : lc_expr ?P |- _ => dependent destruction H; eauto; fail                                                           
+  end.
+
+
+Lemma usub_elab_keeps_mono : forall Γ e1 e2 A Γ' e1' e2' A',
+    usub_elab Γ e1 e2 A Γ' e1' e2' A' ->
+        (mono_type e1 /\ mono_type e2 -> mono_btype e1' /\ mono_btype e2') /\ lc_bexpr e1' /\ lc_bexpr e2'.
+Proof.
+  intros.
+  induction H; repeat split; destruct_conjs; intros; auto; try solve_trivial_mono_or_lc.
+  - dependent destruction H7. dependent destruction H16.
+    constructor.
+    + dependent destruction H8. dependent destruction H18.
+      inst_cofinites_with_new.
+      eapply lc_be_pi_exists with (x1:=x); destruct_conjs; auto.
+    + eapply bmono_lambda with (L:=(L `union` L0 `union` L1)).
+      intros. inst_cofinites_with x. destruct_conjs. intuition.
+  - dependent destruction H7. dependent destruction H16.
+    constructor.
+    + dependent destruction H8. dependent destruction H18.
+      inst_cofinites_with_new.
+      eapply lc_be_pi_exists with (x1:=x); destruct_conjs; auto.
+    + eapply bmono_lambda with (L:=(L `union` L0 `union` L1)).
+      intros. inst_cofinites_with x. destruct_conjs. intuition.
+  - constructor; inst_cofinites_with_new. eapply lc_be_abs_exists with (x1:=x). destruct_conjs. auto.
+    eapply lc_be_pi_exists with (x1:=x); destruct_conjs; auto.
+  - constructor; inst_cofinites_with_new. eapply lc_be_abs_exists with (x1:=x). destruct_conjs. auto.
+    eapply lc_be_pi_exists with (x1:=x); destruct_conjs; auto. 
+  - dependent destruction H5. dependent destruction H13. eapply bmono_pi with (L:=L `union` L0 `union` L1).
+    + intuition. 
+    + intros. inst_cofinites_with x. destruct_conjs. intuition. 
+  - dependent destruction H5. dependent destruction H13. eapply bmono_pi with (L:=L `union` L0 `union` L1).
+    + intuition. 
+    + intros. inst_cofinites_with x. destruct_conjs. intuition. 
+  - inst_cofinites_with_new. eapply lc_be_pi_exists with (x1:=x); destruct_conjs; auto.
+  - inst_cofinites_with_new. eapply lc_be_pi_exists with (x1:=x); destruct_conjs; auto.
+  - dependent destruction H2. dependent destruction H3. constructor; intuition.
+  - dependent destruction H2. dependent destruction H3. constructor; intuition.
+  - dependent destruction H9. dependent destruction H18.
+    econstructor.
+    + inst_cofinites_with_new. eapply lc_be_all_exists with (x1:=x); intuition.
+    + eapply bmono_bind with (L:=L `union` L0 `union` L1). intros. inst_cofinites_with x. intuition.
+  - dependent destruction H9. dependent destruction H18.
+    econstructor.
+    + inst_cofinites_with_new. eapply lc_be_all_exists with (x1:=x); intuition.
+    + eapply bmono_bind with (L:=L `union` L0 `union` L1). intros. inst_cofinites_with x. intuition.
+  - econstructor.
+    + inst_cofinites_with_new. apply lc_be_bind_exists with (x1:=x). destruct_conjs. auto.
+    + inst_cofinites_with_new. apply lc_be_all_exists with (x1:=x); destruct_conjs; auto.
+  - econstructor.
+    + inst_cofinites_with_new. apply lc_be_bind_exists with (x1:=x). destruct_conjs. auto.
+    + inst_cofinites_with_new. apply lc_be_all_exists with (x1:=x); destruct_conjs; auto.
+  - dependent destruction H4. dependent destruction H15. constructor; intuition.
+  - dependent destruction H4. dependent destruction H15. constructor; intuition.
+  - dependent destruction H2. dependent destruction H3. constructor; intuition.
+  - dependent destruction H2. dependent destruction H3. constructor; intuition.
+  - inst_cofinites_with_new. eapply lc_be_all_exists with (x1:=x); destruct_conjs; auto. 
+  - inst_cofinites_with_new. eapply lc_be_all_exists with (x1:=x); destruct_conjs; auto. 
+  - inst_cofinites_with_new. eapply lc_be_all_exists with (x1:=x); destruct_conjs; auto. 
+  - inst_cofinites_with_new. eapply lc_be_all_exists with (x1:=x); destruct_conjs; auto. 
+  - constructor; intuition.
+  - constructor; intuition.
+Qed. 
+
 
 Theorem bidir_complete4 : forall Γ e1 e2 A Γ' e1' e2' A'
   , usub_elab Γ e1 e2 A Γ' e1' e2' A'
@@ -71,15 +139,15 @@ Proof with eauto with bidir.
         eapply bidir_narrowing with (B:=A1'); simpl; eauto...
   - econstructor; eauto... 
   - econstructor; eauto...
-    + admit. (* monotype *)
-    + econstructor; intros.
+    + eapply usub_elab_keeps_mono; eauto.
+    + eapply iapp_pi with (k:=bk_star); intros.
       * admit. (* type_correctness *)
       * eapply bs_sub with (A:=A'0); eauto...
         admit. (* type_correctness *)
   - eapply bs_anno.
-    + eapply bs_bind.
+    + eapply bs_bind with (L:=L).
       * eauto...
-      * admit.  (* type_correctness *)
+      * intros. inst_cofinites_with x. eauto... 
       * intros. eapply bs_sub.
         inst_cofinites_with x. eauto...
         eauto...
@@ -96,10 +164,10 @@ Proof with eauto with bidir.
     + eapply bs_sub with (A:=B'). auto. admit. (* type_correctness *)
   - eapply bs_castdn with (A:=A'0). exact H0. admit. (* breduce *) auto.
   - eapply bs_forall_l with (t:=t'); eauto...
-    + admit. (* monotype *)
+    + eapply usub_elab_keeps_mono; eauto. 
   - econstructor; eauto.
   - eapply bs_forall; eauto.
-  - econstructor.
+  - econstructor. 
     eapply bs_sub with (A:=A'0); eauto.
     + admit. (* bidir_refl_r *)
     + admit. (* bidir_refl_r *)
@@ -108,6 +176,7 @@ Proof with eauto with bidir.
     + admit. (* ctx_dom *)
     + exact H1.
 Admitted.
+
 
 
 (* completeness / totality of elaboration system *)
@@ -168,19 +237,13 @@ Proof.
     + admit. (* ctx_dom *)
   - exists A0, B. auto.
   - destruct H2 as [D [E ]]. exists D, E. destruct_conjs. repeat split; auto.
-    inversion H4.
-    + left. econstructor.
-      * admit.
-      * admit.
-      * eauto.
-      * eauto.
-      * eauto.
-    + left. econstructor.
-      * admit.
-      * admit.
-      * eauto.
-      * rewrite <- H5. admit.
-      * eauto.       
+    inversion H4; left; eapply ott.s_forall_l with (t:=t); eauto.
+    + admit.
+    + admit.
+    + admit.
+    + admit.
+    + rewrite <- H5. admit.
 Admitted.
 
 
+ 
