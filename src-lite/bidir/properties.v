474759829aebc98339659dcc19d87b9c468f16b6
Shengyi Jiang <shengyi.jiang@outlook.com>
2022-04-02 21:43:55 +0800

    add the skeleton of bidir_narrowing

--- src-lite/bidir/properties.v
+++ src-lite/bidir/properties.v
@@ -11,6 +11,21 @@ Scheme Induction for busub Sort Prop
   with Induction for greduce Sort Prop.
 
 
+Lemma bctx_app_cons : forall Γ1 Γ2 x A,
+    Γ1 ,,' Γ2 ,' x : A = Γ1 ,,' (Γ2 ,' x : A).
+Proof.
+  auto.
+Qed.
+
+Hint Rewrite bctx_app_cons : bctx.
+
+Lemma weakening_auto_helper : forall Γ1 Γ2 x A,
+    ⊢ Γ1 ,, Γ2 , x : A -> ⊢ Γ1 ,, (Γ2 , x : A).
+Proof.
+  auto.
+Qed.
+
+
 Lemma check_sub_invert : forall Γ e1 e2 A,
   Γ ⊢ e1 <: e2 ⇐ A ->
   A = ◻' \/ exists B k, Γ ⊢ B <: A ⇒ ⧼ k ⧽' /\ Γ ⊢ e1 <: e2 ⇒ B.
@@ -167,7 +182,6 @@ Proof.
   generalize dependent HeqΓ. generalize dependent Γ1. generalize dependent Γ2. generalize dependent Γ3.
   pattern Γ, e1, e2, d, A, H. 
 
-  Print busub_weakening_mut.
   eapply busub_weakening_mut with 
   (P0 := fun Γ A B (_ : Γ ⊢ A ⟼ B) => 
     forall Γ1 Γ2 Γ3, 
@@ -207,14 +221,42 @@ Proof.
     + apply gr_reduce; auto. apply bwf_lc. auto.
     + dependent destruction b.
   - eapply gr_all with (L:=L `union` bctx_dom (Γ1,,'Γ2,,'Γ3)) (t:=t); eauto.
-    intros. weakening_auto.
+    intros; weakening_auto.
 
   (** infer_app **)
   - econstructor; eauto. apply bwf_lc. eauto.
   - dependent destruction H4. 
     intros. eapply iapp_all with (t:=t); eauto.
-Admitted.
+Qed.
 
+
+Theorem bidir_weakening' : forall Γ1 Γ2 Γ3 e1 e2 d A,
+  busub (Γ1,,'       Γ3) e1 e2 d A -> ⫦ Γ1 ,,' Γ2 ,,' Γ3 ->
+  busub (Γ1,,' Γ2,,' Γ3) e1 e2 d A.
+Proof with autorewrite with bctx; eauto.  
+  intros. remember (Γ1,,' Γ3) as Γ. 
+  generalize dependent HeqΓ. generalize dependent Γ3.
+  pattern Γ, e1, e2, d, A, H. 
+
+  eapply busub_weakening_mut with 
+  (P0 := fun Γ A B (_ : Γ ⊢ A ⟼ B) => 
+    forall Γ3, 
+    Γ = Γ1,,' Γ3 ->
+    Γ1,,' Γ2,,' Γ3 ⊢ A ⟼ B
+  )
+  (P1 := fun Γ A F (_ : infer_app Γ A F) => 
+    forall Γ3, 
+    Γ = Γ1,,' Γ3 ->
+    infer_app (Γ1,,'Γ2,,'Γ3) A F
+  ); intros; auto; subst. 
+
+  (** busub **)
+  - constructor. auto. eapply in_bctx_weakening; auto. 
+    eapply lc_bmiddle. apply bwf_lc. eauto.
+  - econstructor. eauto.
+  - eapply bs_abs with (L:=L); eauto; intros; inst_cofinites_with x. eauto...
+    eapply H1; auto. simpl. admit.
+Admitted.
 
 (* Theorem bidir_elab_weakening : forall Γ1' Γ2' Γ3' e1' e2' d k Γ1 Γ2 Γ3 e1 e2,
   busub_elab (Γ1',,'         Γ3') e1' e2' d ⧼k⧽' (Γ1,,      Γ3) e1 e2 ⧼(to_k k)⧽ ->
@@ -224,12 +266,88 @@ Proof.
   intros.
 Admitted. *)
 
-Theorem bidir_narrowing : forall Γ1 x B Γ2 e1 e2 d C,
+Lemma bidir_narrowing_lc_helper : forall Γ1 Γ2 x A B k,
+  lc_bcontext (Γ1,' x : B,,' Γ2) ->
+  Γ1 ⊢ A <: B ⇒ ⧼ k ⧽' ->
+  lc_bcontext (Γ1,' x : A,,' Γ2).
+Proof.
+  intros.
+  replace (Γ1,' x : B,,' Γ2) with (Γ1,' x : B,,' Γ2,,' bctx_nil) in H by auto.
+  apply lc_bmiddle in H. 
+  replace (Γ1,' x : A,,' Γ2) with (Γ1,' x : A,,' Γ2,,' bctx_nil) by auto.
+  apply lc_binsert_middle; apply busub_all_lc in H0; simpl; intuition.
+Qed.
+
+Scheme  busub_narrowing_mut       := Induction for busub       Sort Prop
+  with  wf_bcontext_narrowing_mut := Induction for wf_bcontext Sort Prop
+  with  greduce_narrowing_mut     := Induction for greduce     Sort Prop
+  with  infer_app_narrowing_mut   := Induction for infer_app   Sort Prop.
+
+Theorem bidir_narrowing' : forall Γ1 x B Γ2 e1 e2 d C,
   busub (Γ1,' x : B,,' Γ2) e1 e2 d C -> 
   forall A k, Γ1 ⊢ A <: B ⇒ ⧼ k ⧽' -> 
   busub (Γ1,' x : A,,' Γ2) e1 e2 d C.
-Proof.   
-Admitted.
+Proof with autorewrite with bctx; eauto.
+  intros. remember (Γ1,' x : B,,' Γ2) as Γ.
+  generalize dependent HeqΓ. generalize dependent Γ2. 
+  pattern Γ, e1, e2, d, C, H.
+  Check busub_narrowing_mut.
+  
+  eapply busub_narrowing_mut with 
+  (P := fun Γ e1 e2 d C (_ : busub Γ e1 e2 d C) =>
+    forall Γ2, 
+     Γ = Γ1 ,' x : B ,,' Γ2 ->
+     busub (Γ1 ,' x : A ,,' Γ2) e1 e2 d C
+  )
+  (P0 := fun Γ (_ : wf_bcontext Γ) => 
+    forall Γ2, 
+      Γ = Γ1,' x : B,,' Γ2 -> 
+      ⫦ Γ1,' x : A,,' Γ2
+  )
+  (P1 := fun Γ C D (_ : Γ ⊢ C ⟼ D) => 
+    forall Γ2, 
+      Γ = Γ1,' x:B,,' Γ2 ->
+      Γ1,' x : A,,' Γ2 ⊢ C ⟼ D
+  )
+  (P2 := fun Γ C F (_ : infer_app Γ C F) =>
+    forall Γ2, 
+      Γ = Γ1,' x:B,,' Γ2 ->
+      infer_app (Γ1,' x : A,,' Γ2) C F
+  )
+  ; intros; auto; subst.
+  - constructor; auto. admit.
+  - eauto.
+  - eapply bs_abs with (L:=L); eauto; intros; inst_cofinites_with x0; eauto... 
+  - eapply bs_pi with (L:=L); eauto; intros; inst_cofinites_with x0; eauto...
+  - eauto. 
+  - eapply bs_bind with (L:=L); eauto; intros; inst_cofinites_with x0; eauto...
+  - eauto.
+  - eauto. 
+  - eapply bs_forall_l with (L:=L); eauto; intros; inst_cofinites_with x0; eauto...
+  - eapply bs_forall_r with (L:=L); eauto.
+    intros. inst_cofinites_with x. eauto...
+  - eapply bs_forall with (L:=L); eauto; intros.
+    inst_cofinites_with x. eauto...
+  - eauto.
+  - eauto.
+
+  (* P0 *)
+  - destruct Γ2; inversion H1.
+  - destruct Γ2.
+    + dependent destruction H3. simpl. econstructor; auto.
+      eapply bidir_refl_l in H0. eauto.
+    + dependent destruction H3. simpl. econstructor; eauto.
+    admit.
+
+  (* P1 *)
+  - dependent destruction b; econstructor; eauto; eapply bidir_narrowing_lc_helper; eauto.
+  - eapply gr_all with (L:=L); eauto. intros.
+    inst_cofinites_with x0. eauto...
+
+  (* P2 *)
+  - econstructor; eauto. eapply bidir_narrowing_lc_helper; eauto.
+  - econstructor; eauto.
+Admitted. 
 
 
 Hint Resolve bidir_narrowing : bidir.
